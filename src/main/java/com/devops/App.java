2bfca231919e83d8b5f1acc6d38c56b107acb034
Sree Prasanth J <2022wb86044@wilp.bits-pilani.ac.in>
2026-09-25 14:07:52 +0530

    Update application message for CI test

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -2,6 +2,6 @@ package com.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("DevOps Lab - Jenkins Continuous Integration");
+        System.out.println("DevOps Lab - Jenkins Continuous Integration - Updated");
     }
 }
